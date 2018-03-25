50c8c5ffd202c8fd3f8e0aafc9ed417959949aa0
Kyle <kylelprince@gmail.com>
2018-03-25 14:10:14 -0400

    Got started with the Vertical prototype

---
+++ WitchHunt/register.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Witch Hunt Sign In</title>
+  </head>
+  <body>
+    <?php echo "You Registered..." ?>
+  </body>
+</html>
---
+++ WitchHunt/signIn.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Witch Hunt Sign In</title>
+  </head>
+  <body>
+    <?php echo "You signed In..." ?>
+  </body>
+</html>
