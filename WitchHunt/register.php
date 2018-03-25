5f9a9f1371f41965127bcab7b5df6cfa7b16af97
Kyle <kylelprince@gmail.com>
2018-03-25 14:33:34 -0400

    Added Backend Code
    Sign In/Sign Up functionality

--- WitchHunt/register.php
+++ WitchHunt/register.php
@@ -1,9 +1,42 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Witch Hunt Sign In</title>
+    <title>Witch Hunt - Register</title>
   </head>
   <body>
-    <?php echo "You Registered..." ?>
+    <?php
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    $vpassword = $_GET["verified_pass"];
+
+    if ($password != $vpassword) {
+      echo "Passwords must match!";
+    } else {
+      $conn = new mysqli("localhost", "CEN4010_S2018g04", "cen4010_s2018", "CEN4010_S2018g04");
+      $statement = "SELECT * FROM users WHERE username = '$username';";
+      if (!$result = $conn->query($statement)) {
+        echo "Cannot register <br>";
+        echo "Query: " . $statement . "<br>";
+        echo "Errno: " . $conn->errno . "<br>";
+        echo "Error: " . $conn->error . "<br>";
+      } else {
+        if ($result->num_rows == 0) {
+          $insert = "INSERT INTO users (username, password) VALUES ('$username', '$password');";
+          if ($conn->query($insert)) {
+            $conn->Close();
+            //$page = file_get_contents("list.php");
+            //echo $page;
+            //$_SESSION["username"] = $username;
+            //$_SESSION["password"] = $password;
+            //include("list.php");
+            echo "You registered!!"
+          } else {
+            echo "Query didn't work.";
+            $conn->Close();
+          }
+        }
+      }
+    }
+    ?>
   </body>
 </html>
--- WitchHunt/signIn.php
+++ WitchHunt/signIn.php
@@ -1,9 +1,30 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Witch Hunt Sign In</title>
+    <title>Witch Hunt - Sign In</title>
   </head>
   <body>
-    <?php echo "You signed In..." ?>
+    <?php
+
+    $user = $_GET["username"];
+    $pass = $_GET["password"];
+
+    $con = new mysqli("localhost", "CEN4010_S2018g04", "cen4010_s2018", "CEN4010_S2018g04");
+    $statement = "SELECT * FROM users WHERE username = '$user' AND password = '$pass';";
+    if (!$result = $con->query($statement)) {
+      echo "Cannot call a query!";
+    } else {
+      if ($result->num_rows > 0) {
+        $_SESSION["username"] = $user;
+        $_SESSION["password"] = $pass;
+        $con->Close();
+        //$page = file_get_contents("list.php");
+        //echo $page;
+        //include("list.php");
+        echo "You signed in!!";
+      }
+    }
+    ?>
   </body>
 </html>
