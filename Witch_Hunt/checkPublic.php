71a88ec9e2e430a1c7f018add2c3275e73dbeeb2
imaykov <35824666+imaykov@users.noreply.github.com>
2018-04-15 21:16:22 -0400

    Add files via upload

--- Witch_Hunt/checkPublic.php
+++ Witch_Hunt/checkPublic.php
@@ -1,5 +1,20 @@
 <?php
+require_once './includes/config.php'; 
+// This checks to make sure user is logged in, and redirects to index.php if not
+require_once './includes/logged_in.php';
+$username=$_SESSION["username"];
 
+$statement='SELECT * FROM waiting_for_game WHERE username="'.$username.'";';
 
+if (!$result = $sqlcon->query($statement)) {
+      echo "Error fetching game data!";
+    } else {
+      if($result->num_rows<1){
+        echo 1;
+      }
+      else{
+        echo 0;
+      }
+    }
 
 ?>
--- Witch_Hunt/waiting.php
+++ Witch_Hunt/waiting.php
@@ -2,6 +2,33 @@
 require_once './includes/config.php'; 
 // This checks to make sure user is logged in, and redirects to index.php if not
 require_once './includes/logged_in.php'; 
+
+$newPlace=1;
+$statement1="SELECT max(place) FROM waiting_for_game;";
+if (!$result = $sqlcon->query($statement1)) {
+      echo "Error fetching game data!1";
+    }
+    else{
+      if ($result->num_rows>0){
+        $newPlace=mysqli_fetch_array($result)[0]+1;
+      }
+      echo $newPlace;
+    }
+
+try{
+$statement2="insert into waiting_for_game 
+values('any',
+'".$_SESSION['username']."',
+0,
+0,
+5,
+".$newPlace.");";
+
+$sqlcon->query($statement2);
+}
+catch (Exception $e){
+    echo $e->getMessage();
+}
 ?>
 
 
@@ -16,16 +43,10 @@ require_once './includes/logged_in.php';
           xmlRequest.open("POST","checkPublic.php",false);
           xmlRequest.send();
           if(xmlRequest.responseText==1){
-            window.location=game.php;
+            window.location="http://lamp.cse.fau.edu/~CEN4010_S2018g04/WitchHuntTest001/game.php";
           }
       }
-      
-      function AddtoWaitlist(){
-          var xmlRequest=new XMLHttpRequest();
-          
-          xmlRequest.open("POST","addtoPublic.php",false);
-          xmlRequest.send('gamename="any"');
-      }      
+         
     </script>
   </head>
   <body>
