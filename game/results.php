c8ca88ec2a47d1da2bcdc47b5f701757dd0e45de
imaykov <35824666+imaykov@users.noreply.github.com>
2018-04-16 20:34:30 -0400

    Add files via upload

--- game/results.php
+++ game/results.php
@@ -17,14 +17,13 @@ require_once './includes/logged_in.php';
   $statement = 'SELECT * FROM game_players WHERE username="'.$username.'" and alive=1;';
    if (!$result = $sqlcon->query($statement)) {
 	   echo "error";
-	   
    }
-   else{
+   else{//big else start
 	 if ($result->num_rows > 0) {
 		$array=$result->fetch_assoc();
 		$isWitch=$array["witch"];
 	    $gameID=$array["id"];
-   }
+     }
   
   
   
@@ -37,17 +36,21 @@ require_once './includes/logged_in.php';
   $sqlcon->query($killVote);
   $checkWitch='SELECT * FROM game_players WHERE witch=1 and alive=1 and id'.$gameID.';';
   $testWin=$sqlcon->query($checkWitch);
-  if ($testWin && $testWin->num_rows>0){
+  //if witch is alive
+  if ($testWin && $testWin->num_rows>0){//if1 start
 	  $killW='UPDATE game_players SET alive=0 WHERE wvote=1;';
 	  $testAlive='SELECT * FROM game_players WHERE alive=1;';
-	  $resultAlive=sqlcon->query($testAlive);
+	  $sqlcon->query($killW);
+	  $resultAlive=$sqlcon->query($testAlive);
+	  
+	  //if the witch has not won
 	  if ($resultAlive && $resultAlive->num_rows<3){
 		echo '<h3>The witch has won!</h3>';
 		echo '<form action="home.php">';
 		echo '<input type="submit" value="Continue">';
 		echo '</form>';	  
 	  }
-	  
+	  //if the witch has not won
 	  else{
 		  $statement2='SELECT * FROM game_players WHERE username="'.$username.'" and alive=1;';
 		  
@@ -55,13 +58,15 @@ require_once './includes/logged_in.php';
 		  
 		  $voteZero="UPDATE game_players SET vote=0, round=round+1,wvote=0;";
 		  
-		  $result=sqlcon->query($statement2);
+		  $result=$sqlcon->query($statement2);
+		  //you are alive st
 		  if ($result && $result->num_rows>0){
 			echo '<h3>You are still alive</h3>';
 			echo '<form action="votev2.php">';
 			echo '<input type="submit" value="Continue">';
 			echo '</form>';
 		  }
+		  //you are dead statement
 		  else{
 			echo '<h3>You are dead</h3>';
 			echo '<form action="home.php">';
@@ -69,15 +74,14 @@ require_once './includes/logged_in.php';
 			echo '</form>';	  
 		  }
 	  }
-   }
-   
-   }
+   }//if1 end
    else if ($testWin && $testWin->num_rows==0){
-	echo '<h3>The witch is dead</h3>';
-	echo '<form action="home.php">';
-	echo '<input type="submit" value="Continue">';
-	echo '</form>';	
+		echo '<h3>The witch is dead</h3>';
+		echo '<form action="home.php">';
+		echo '<input type="submit" value="Continue">';
+		echo '</form>';	
    }
+   }//big else end
   ?>
   </body>
 </html>
