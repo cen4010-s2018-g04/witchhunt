2a17a2694f97113c3773f1d2f376123774728406
shaquanajones2013 <35879786+shaquanajones2013@users.noreply.github.com>
2018-05-01 13:38:11 -0400

    Update head.php

--- Witch_Hunt_v2/includes/head.php
+++ Witch_Hunt_v2/includes/head.php
@@ -3,3 +3,41 @@
     var minplay = <?php echo json_encode($numplayers); ?>;
 </script>
 <script src="./js/witchhunt.js"></script>
+
+<!DOCTYPE html>
+<html>
+  <head>
+          <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style>
+        body{
+            background-image: url("http://www.toptenz.net/wp-content/uploads/2011/10/Halloween-Witches.jpg")
+        }
+    </style>
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+          <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <body style="text-align:center;">Centered Heading</body>
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Witch Hunt</a>
+            </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about">Instructions</a></li>
+                    <li><a href="#contact">Standings</a></li>
+                </ul>
+                
+            </div>
+          </nav>
+              <br><br><br><br><br><br>
+        </body>
+      </html>
